489d754d3e40e593a6de74c65cfbdc21c3fefa06
Tianwei Liu <tianwei.liu89@gmail.com>
2015-03-30 22:53:03 -0400

    added unity and client basic communication

--- index.js
+++ index.js
@@ -6,15 +6,84 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
-server.listen(80);
+//Unity namespace
+var unity = io.of('/unity');
+//Unity socket
+var unitySocket;
+
+//Client namespace
+var client = io.of('/client');
+
+//User list
+var users = [];
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+server.listen(app.get('port'), function() {
+	console.log('Node app is running at localhost:' + app.get('port'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
+/* Unity communication */
+
+unity.on('connection', function(socket) {
+	//Unity connected
+	socket.on('unityConnected', function() {
+		unitySocket = socket;
+		unity.emit('unityConnected');
+	});
+
+	//Unity disconnected
+	socket.on('disconnect', function() {
+		if (socket == unitySocket) {
+			unitySocket = null;
+			unity.emit('unityDisconnected');
+		}
+	});
+
+	//Unity update
+	socket.on('unityUpdate', function(data) {
+		client.emit('unityUpdate', data);
+	});
+
+
+});
+
+/* Client communication */
+
+client.on('connection', function(socket) {
+	//User logs in
+	socket.on('login', function(name, email) {
+		users[socket] = {
+			'name': name,
+			'email': email
+		};
+	});
+	//User logs out / disconnects
+	socket.on('disconnect', function() {
+		if (socket in users) {
+			if (users[socket].name != '')
+				client.emit('chat', {
+					'name': null,
+					'message': users[socket].name + ' has left.'
+				});
+			else
+				client.emit('chat', {
+					'name': null,
+					'message': 'Anonymous user has left.'
+				});
+			delete users[socket];
+		}
+		else
+			console.error('Error: disconnected user is not in the user list.')
+	});
+	//Chat message
+	socket.on('chat', function(data) {
+		client.emit('chat', data);
+	});
+
+	//Add blob
+	socket.on('addBlob', function(data) {
+		unity.emit("addBlob", data);
+	});
 });
