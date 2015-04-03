b459d768629296985f89e5f560e72c0cbdb28047
Tianwei Liu <tianwei.liu89@gmail.com>
2015-04-03 00:41:32 -0400

    Revert "test cross-origin setting"
    This reverts commit 34aac188f270d7fa96720f97a3a3fa3b382afd91.

--- index.js
+++ index.js
@@ -21,13 +21,6 @@ var users = [];
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  return next();
-});
-
 server.listen(app.get('port'), function() {
 	console.log('Node app is running at localhost:' + app.get('port'));
 });
